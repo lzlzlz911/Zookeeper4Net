46c06d07056db6706f6f68cf59fb56c5d0cb0a7c
lzlzlz911 <lzlzlz911@hotmail.com>
2017-03-16 11:16:21 +0800

    Add project files.

---
+++ ZKDeemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+namespace ZKDeemo
+{
+    #region Using
+
+    using System;
+
+    using ZooKeeperNet;
+
+    #endregion
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", new TimeSpan(0, 0, 0, 5000), new TWatch()))
+            {
+
+                var stat = zk.Exists("/root", true);
+
+                var stat2 = zk.Exists("/root/childone", true);
+
+                var childs = zk.GetChildren("/root", false);
+
+                //zk.Create("/root/lz", "/root/lz".GetBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
+
+                //zk.Create("/root/childone", "/root/childone".GetBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
+
+                //zk.GetChildren("/root", true);
+
+                //zk.GetData("/root", true, null);
+
+                //zk.SetData("/root/childone", "childonemodify".GetBytes(), -1);
+
+                //zk.Delete("/root/lz", -1);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ZKDeemo/TWatch.cs
@@ -0,0 +1,21 @@
+﻿namespace ZKDeemo
+{
+    #region Using
+
+    using System;
+
+    using ZooKeeperNet;
+
+    #endregion
+
+    public class TWatch : IWatcher
+    {
+        public void Process(WatchedEvent @event)
+        {
+            if (@event.Type == EventType.NodeDataChanged)
+            {
+                Console.WriteLine(@event.Path);
+            }
+        }
+    }
+}
